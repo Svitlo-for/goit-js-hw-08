5fdd89aed5f6d3b84dbcc94eec28179eab6f7236
Svitlo-for <serg_machigin@ukr.net>
2023-08-13 09:56:29 +0300

    add-task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
+
+
+
+function onPlay( {seconds} ) {
+    localStorage.setItem(STORAGE_KEY, seconds);
+};
+
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+
+if (localStorage.getItem(STORAGE_KEY)) {
+    player.setCurrentTime(localStorage.getItem(STORAGE_KEY))
+}
+
+
+// const iframe = document.querySelector('iframe');
+// const player = new Player(iframe);
+
+// player.on('timeupdate', throttle(onPlay, 1000));
+
+// function onPlay(data) {
+//     localStorage.setItem('videoplayer-current-time', data.seconds);
+// };
+
+// const currentTimeValue = localStorage.getItem('videoplayer-current-time') || 0;
+// player.setCurrentTime(currentTimeValue);
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from "lodash.throttle";
+const form = document.querySelector(".feedback-form");
 
+let emailValue = ' ';
+let messageValue = ' ';
+
+const data = {
+    email: emailValue,
+    message: messageValue,
+}
+
+const savedData = JSON.parse(localStorage.getItem("feedback-form-state"));
+if (savedData) {
+    form.elements.email.value = savedData.email;
+    data.message.email.value = savedData.message;
+}
+
+form.addEventListener('input', throttle(onInput, 500));
+
+function onInput() {
+    emailValue = form.elements.email.value;
+    messageValue = form.message.email.value;
+    localStorage.setItem("feedback-form-state", JSON.stringify(data));
+}
+
+form.addEventListener('submit', onSubmit);
+
+function onSubmit() {
+    localStorage.removeItem("feedback-form-state");
+    reset(form);
+    console.log(data);
+}
+
+window.addEventListener('load', onLoad);
+
+function onLoad() {
+    localStorage.getItem(data)
+}
